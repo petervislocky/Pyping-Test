0c9d5680c2ad07232e3c90028e3330e9ff7afab3
Peter <lilv447@gmail.com>
2025-07-11 00:01:15 -0400

    added docs

--- input_handler.py
+++ input_handler.py
@@ -1,5 +1,7 @@
-
 def capture_typing(term):
+    """takes a blessed terminal object and uses cbreak to capture user keystrokes,
+    if user presses esc, returns None, else it yields each keystroke one. This method
+    becomes an iterable because of the yield"""
     with term.cbreak():
         while True:
             key = term.inkey()
@@ -8,6 +10,9 @@ def capture_typing(term):
             yield key
 
 def input_list(term):
+    """takes a blessed terminal object to pass to capture_typing, iterates through all keys
+    returned from capture_typing and appends them to a list, if backspace is pressed it pops
+    the list, returns the list of typed text and count of backspace pressed"""
     typed_text = []
     backspace_pressed = 0
     for key in capture_typing(term):
