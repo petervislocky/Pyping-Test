6035db9a0cab7fb1da6c24cdaf73c8deb4dd6f04
Peter <lilv447@gmail.com>
2025-11-01 22:21:22 -0400

    idk

--- typing_test/modes/perfect_mode.py
+++ typing_test/modes/perfect_mode.py
@@ -53,7 +53,7 @@ def run_perfect_mode(
     time_elapsed_sec = end_time - start_time
     time_elapsed_min = time_elapsed_sec / 60
 
-    console.print(f"[bold green]Time to complete:[/] {int( time_elapsed_sec )} seconds")
+    console.print(f"[bold green]Time to complete:[/] {int(time_elapsed_sec)} seconds")
     console.print(
         f"[bold red]Words per minute:[/] {metrics.wpm(len(reference_text), time_elapsed_min):.2f} wpm (words per minute)"
     )
